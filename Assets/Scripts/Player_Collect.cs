dcb812b18e3abe832113cd90e6f25a112e96682f
J0ujou <julie12manceau@gmail.com>
2025-09-16 18:19:30 +0200

    modification sur le gain de point de Soft

--- Assets/Scripts/Player_Collect.cs
+++ Assets/Scripts/Player_Collect.cs
@@ -5,6 +5,6 @@ public class Player_Collect : MonoBehaviour
   [SerializeField] private ScoreDatas _scoreData;
   public void UpdateScore(int value)
   {
-    _scoreData.ScoreValue += Mathf. Clamp(_scoreData.ScoreValue + value, 0, _scoreData.ScoreValue+ value);
+    _scoreData.ScoreValue = Mathf. Clamp(_scoreData.ScoreValue + value, 0, _scoreData.ScoreValue+ value);
   }
 }
--- Assets/Scripts/Target_Soft.cs
+++ Assets/Scripts/Target_Soft.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 public class Target_Soft : MonoBehaviour
 { 
-    [SerializeField] private int _targetValueSo = 1;
+    [SerializeField] private int _targetValue = 1;
     private void OnTriggerEnter(Collider other)
     { 
         if (other.gameObject.GetComponent<Player_Collect>() != null)
         {
-            other.gameObject.GetComponent<Player_Collect>().UpdateScore(_targetValueSo);
+            other.gameObject.GetComponent<Player_Collect>().UpdateScore(_targetValue);
             Destroy(gameObject);
         }
     }
