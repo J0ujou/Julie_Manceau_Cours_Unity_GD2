0c6050e5c0edd09b2feb8de280892ec6cde82f87
J0ujou <julie12manceau@gmail.com>
2025-10-09 15:09:02 +0200

    Raycast Grappin

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     private float _horizontalMovement;
     private float _verticalMovement;
     private Vector3 _movement;
+    private Vector3 _GrappinDirection;
+    private Vector3 _GrappinHit;
     [SerializeField] private float _speed = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
         _horizontalMovement = Input.GetAxis("Horizontal");
         _verticalMovement = Input.GetAxis("Vertical");
         _movement = new Vector3(_horizontalMovement, 0.0f, _verticalMovement);
+        GrappinUpdateDirection(_movement);// Mise à jour de la direction de tir du grappin
         _movement.Normalize();
         _movement *= _speed;
         _movement.y = _rb.linearVelocity.y;
@@ -33,6 +36,41 @@ public class PlayerMovement : MonoBehaviour
         {
             Debug.LogError("No RigidBody found");
         }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            TryThrowGrappin();
+        }
+
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            ThrowGrappin();
+        }
         
+        
+    }
+
+    private void GrappinUpdateDirection(Vector3 direction)
+    {
+        if (direction.sqrMagnitude > 0.1f)
+        {
+            _GrappinDirection = direction;
+        }
+    }
+
+    private void TryThrowGrappin()
+    {
+        RaycastHit Hit;
+        if (Physics.Raycast(transform.position, _GrappinDirection, out Hit, 100f))
+        {
+            Debug.DrawRay(transform.position, transform.position+_GrappinDirection*100f, Color.red);
+            _GrappinHit  = Hit.point + Hit.normal*1.5f;
+        }
+    }
+
+    private void ThrowGrappin()
+    {
+       transform.position = _GrappinHit;
+       _GrappinDirection= Vector3.zero;
     }
 }
