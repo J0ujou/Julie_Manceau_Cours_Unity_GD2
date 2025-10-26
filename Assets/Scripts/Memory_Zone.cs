abd7b9cf4e7b8ca9531bd9786e1383000aa214dc
J0ujou <julie12manceau@gmail.com>
2025-10-26 16:04:17 +0100

    avancement memoryzone

--- Assets/Scripts/Memory_Zone.cs
+++ Assets/Scripts/Memory_Zone.cs
@@ -7,8 +7,9 @@ public class Memory_Zone : MonoBehaviour
 
     void OncollisionEnter(Collision other)
     {
-        if (other.gameObject.GetComponent<Player_Collect>() != null && keycollected == true)
+        if (other.gameObject.GetComponent<Player_Collect>()!= null && keycollected == true)
         {
+            Debug.Log("ZONE");
             //TO DO: appeller une fonction dans target fragment qui reaffiche tout les target fragment pendant 5 sec
         }
     }
--- Assets/Scripts/Target_Key.cs
+++ Assets/Scripts/Target_Key.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Target_Key : MonoBehaviour
 {
     [SerializeField] private int _targetValue = 1;
+    [SerializeField] Memory_Zone _memoryZone;
     private void OnCollisionEnter(Collision other)
     {
       if (other.gameObject.GetComponent<Player_Collect>() != null)
       {
           other.gameObject.GetComponent<Player_Collect>().UpdateScorekey(_targetValue);
           Destroy(gameObject);
+          _memoryZone.keycollected = true;
+          Debug.Log("key collected");
       }
     }
 }
